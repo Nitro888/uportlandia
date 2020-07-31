766aa3ff13b993e2d438881c059eb3f5c232b365
kryptojet <63332056+kryptojet@users.noreply.github.com>
2020-07-31 21:10:35 +0900

    Updates

---
+++ precache-manifest.1afa0bc73d39dfbe3e9891ac547e6ab3.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "b8ab6295e5bb96949065",
+    "url": "/Dr.Who/static/css/main.45e713d8.chunk.css"
+  },
+  {
+    "revision": "b8ab6295e5bb96949065",
+    "url": "/Dr.Who/static/js/main.3fb39df9.chunk.js"
+  },
+  {
+    "revision": "233455247ae25944d11a",
+    "url": "/Dr.Who/static/js/runtime~main.5ee4287a.js"
+  },
+  {
+    "revision": "b603c0dd9c6a52b73dad",
+    "url": "/Dr.Who/static/js/2.090c9482.chunk.js"
+  },
+  {
+    "revision": "09cfe256212b76eed2c5e1cbde567ed0",
+    "url": "/Dr.Who/static/media/loading.09cfe256.svg"
+  },
+  {
+    "revision": "066d8846ccbbc22a75bafbf6c89a6a9b",
+    "url": "/Dr.Who/static/media/reload.066d8846.svg"
+  },
+  {
+    "revision": "6b9bf7bb2608ab3ff694cb65554fffd6",
+    "url": "/Dr.Who/static/media/uport-logo.6b9bf7bb.svg"
+  },
+  {
+    "revision": "baee24254676badffe8a3e9df237a794",
+    "url": "/Dr.Who/static/media/itunes.baee2425.svg"
+  },
+  {
+    "revision": "00fc1c084d9ac202886c1768dbdd76df",
+    "url": "/Dr.Who/static/media/playstore.00fc1c08.svg"
+  },
+  {
+    "revision": "37b638cc8f9e0253f12bf430911c028b",
+    "url": "/Dr.Who/static/media/arrow-left.37b638cc.svg"
+  },
+  {
+    "revision": "71bd417f0b4208d1d52aeac1c49ea335",
+    "url": "/Dr.Who/static/media/NHIS_logo.71bd417f.png"
+  },
+  {
+    "revision": "6cfca76ff30bbf5ceb4ceeb0c6574d1e",
+    "url": "/Dr.Who/static/media/uport-icon-white.6cfca76f.svg"
+  },
+  {
+    "revision": "8d28001e0891ddeaf5492dc76681d8da",
+    "url": "/Dr.Who/static/media/wave.8d28001e.svg"
+  },
+  {
+    "revision": "fee7033cfa58536bdbca954d9594dfa1",
+    "url": "/Dr.Who/static/media/city-id-icon.fee7033c.svg"
+  },
+  {
+    "revision": "4ec3278f3a02cab1a30c575990d80fbb",
+    "url": "/Dr.Who/static/media/uport-app-icon.4ec3278f.svg"
+  },
+  {
+    "revision": "18cf63216ed12ed2dd1803618239ae68",
+    "url": "/Dr.Who/static/media/services-icon.18cf6321.svg"
+  },
+  {
+    "revision": "2824005735918aa127003ee4cd246975",
+    "url": "/Dr.Who/static/media/dummy-content-1.28240057.svg"
+  },
+  {
+    "revision": "4547cb485be11921726db30452f257bc",
+    "url": "/Dr.Who/static/media/dummy-content-2.4547cb48.svg"
+  },
+  {
+    "revision": "66e7f9d6d0c722e3ce37785476030c0d",
+    "url": "/Dr.Who/static/media/dummy-content-3.66e7f9d6.svg"
+  },
+  {
+    "revision": "f5a9f90c7cdf78be5c1f28f6b115e83c",
+    "url": "/Dr.Who/static/media/dummy-content-4.f5a9f90c.svg"
+  },
+  {
+    "revision": "732b78bd2816d80ae31fe216391bd450",
+    "url": "/Dr.Who/static/media/angle-icon.732b78bd.svg"
+  },
+  {
+    "revision": "5a6ec01cf762d6ec1107a9b5ae482dee",
+    "url": "/Dr.Who/static/media/secure-icon.5a6ec01c.svg"
+  },
+  {
+    "revision": "2d30e218d759bd6c599c4e0ea3a9d75b",
+    "url": "/Dr.Who/static/media/smiley-face-city.2d30e218.svg"
+  },
+  {
+    "revision": "f94a720623c83ac97cdc2bbe14d9eded",
+    "url": "/Dr.Who/static/media/accept-attestation.f94a7206.svg"
+  },
+  {
+    "revision": "57ba07c42b2ade791c634ae159c51da4",
+    "url": "/Dr.Who/static/media/congratulations-city.57ba07c4.svg"
+  },
+  {
+    "revision": "1cc7f43c0cd3ac75d3d1f8ba0a5df04e",
+    "url": "/Dr.Who/static/media/dev-icon.1cc7f43c.svg"
+  },
+  {
+    "revision": "4a871205c5e9fd5734803cfe54b2125f",
+    "url": "/Dr.Who/static/media/citizen-icon.4a871205.svg"
+  },
+  {
+    "revision": "df53d6421dd342cfa55f0a3251c941bc",
+    "url": "/Dr.Who/static/media/popup-arrow.df53d642.svg"
+  },
+  {
+    "revision": "e5f8473981cf1cd41b118d504dd9bd7c",
+    "url": "/Dr.Who/static/media/grumpy-face.e5f84739.svg"
+  },
+  {
+    "revision": "ee6b2d9776d0c71268c09df98079a56d",
+    "url": "/Dr.Who/static/media/smiley-face-diploma.ee6b2d97.svg"
+  },
+  {
+    "revision": "1460b946b59c59f0ed3d6db60a00e41f",
+    "url": "/Dr.Who/index.html"
+  }
+];
